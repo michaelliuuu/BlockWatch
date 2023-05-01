55aae306ce496dc1dc15c833b18367932b7e19a0
Michael Liu <102439915+michaelliuuu@users.noreply.github.com>
2023-05-01 11:46:03 -0700

    Clicking on the crypto text now enlarges container and will eventually display a graph

--- popup.js
+++ popup.js
@@ -44,44 +44,64 @@ document.addEventListener('DOMContentLoaded', function() {
           console.error('Error fetching Bitcoin price:', error);
           message2.textContent = 'Error fetching Bitcoin price';
         });
-    }
+  }
   
     //display fear and greed index
-    function checkFearAndGreedIndex() {
-      fetch(fearAndGreedIndexUrl)
-        .then(response => response.json())
-        .then(data => {
-          const fearAndGreedIndex = data.data[0].value;
-          if(fearAndGreedIndex > 55 && fearAndGreedIndex <= 75) {
-            message1.textContent = `It's not a good time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 55-75.`;
-            thumb.src = 'images/down.png';
-          } 
-          else if(fearAndGreedIndex > 45 && fearAndGreedIndex <= 55){
-            message1.textContent = `It's an okay time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 45-55.`;
-            thumb.src = 'images/up.png';
-          }
-          else if(fearAndGreedIndex > 25 && fearAndGreedIndex <= 45) {
-            message1.textContent = `It's a good time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 25-45.`;
-            thumb.src = 'images/up.png';
-          }
-          else if(fearAndGreedIndex => 0 && fearAndGreedIndex <= 25) {
-            message1.textContent = `It's a very good time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 0-25.`;
-            thumb.src = 'images/up.png';
-          }
-          else if(fearAndGreedIndex > 75 && fearAndGreedIndex <= 100) {
-            message1.textContent = `It's a very bad time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 75-100.`;
-            thumb.src = 'images/down.png';
-          }
-        })
-        .catch(error => {
-          message.textContent = 'Error fetching Crypto Fear and Greed Index.';
+  function checkFearAndGreedIndex() {
+    fetch(fearAndGreedIndexUrl)
+      .then(response => response.json())
+      .then(data => {
+        const fearAndGreedIndex = data.data[0].value;
+        if(fearAndGreedIndex > 55 && fearAndGreedIndex <= 75) {
+          message1.textContent = `It's not a good time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 55-75.`;
+          thumb.src = 'images/down.png';
+        } 
+        else if(fearAndGreedIndex > 45 && fearAndGreedIndex <= 55){
+          message1.textContent = `It's an okay time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 45-55.`;
+          thumb.src = 'images/up.png';
+        }
+        else if(fearAndGreedIndex > 25 && fearAndGreedIndex <= 45) {
+          message1.textContent = `It's a good time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 25-45.`;
+          thumb.src = 'images/up.png';
+        }
+        else if(fearAndGreedIndex => 0 && fearAndGreedIndex <= 25) {
+          message1.textContent = `It's a very good time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 0-25.`;
+          thumb.src = 'images/up.png';
+        }
+        else if(fearAndGreedIndex > 75 && fearAndGreedIndex <= 100) {
+          message1.textContent = `It's a very bad time to buy crypto. \nThe Fear and Greed Index is ${fearAndGreedIndex} and within \nthe Greed range of 75-100.`;
+          thumb.src = 'images/down.png';
+        }
+      })
+      .catch(error => {
+        message.textContent = 'Error fetching Crypto Fear and Greed Index.';
       });
-  }
+    }
     
   //Calls functions
   updateEtheriumPrice();
   updateBitCoinPrice();
   checkFearAndGreedIndex();
-  setInterval(updatePrice, 60000); // Update the price every minute
   setInterval(checkFearAndGreedIndex, 60000);
+});
+
+//Event Listener that opens graph
+const container = document.querySelector('.container');
+const ethButton = document.querySelector('.currentEtheriumPrice');
+const bitcoinButton = document.querySelector('.currentBitcoinPrice');
+ethButton.addEventListener('click', () => {
+  if(container.style.height === '300px') {
+    container.style.height = '75px';
+  }
+  else {
+    container.style.height = '300px';
+  }  
+});
+bitcoinButton.addEventListener('click', () => {
+  if(container.style.height === '300px') {
+    container.style.height = '75px';
+  }
+  else {
+    container.style.height = '300px';
+  }  
 });
